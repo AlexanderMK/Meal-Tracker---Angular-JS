1ad47cd91b5a4d491db58f4878e66653fff21db8
Alexander <mburukungu@gmail.com>
2017-07-07 15:17:16 +0300

    specified new id, high/low

---
+++ app/new-task.component.ts
@@ -0,0 +1,30 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Task } from './task.model';
+
+@Component({
+  selector: 'new-meal',
+  template: `
+    <h1>New Meal</h1>
+    <div>
+      <label>Enter Meal Description:</label>
+      <input #newDescription>
+    </div>
+    <div>
+      <label>Enter High/Low Calories:</label>
+      <input #newId>
+      <button (click)="
+        addClicked(newDescription.value, newId.value);
+        newDescription.value='';
+        newId.value='';
+      ">Add</button>
+    </div>
+  `
+})
+
+export class NewTaskComponent {
+  @Output() newTaskSender = new EventEmitter();
+  addClicked(description: string, id: string) {
+    var newTaskToAdd: Task = new Task(description, id);
+    this.newTaskSender.emit(newTaskToAdd);
+  }
+}
